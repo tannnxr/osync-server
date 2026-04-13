64e0076165b3d95bda2b31e8f8afca2d38bf9c0e
Tanner <104153588+tannnxr@users.noreply.github.com>
2026-04-13 16:22:41 -0400

    initial upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Response, status
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/health")
+def health_check():
+    return Response(content="OK", status_code=200)
+
